7fcfb79802a50798201ba875be9b12a9686e2228
ShiningVision <antonchen00@gmx.at>
2023-03-28 22:08:02 +0200

    frequency and threshhold slider

--- tests/csv_reader_test.py
+++ tests/csv_reader_test.py
@@ -4,4 +4,4 @@ from mining_algorithms.csv_reader import read
 class TestCsvReader(unittest.TestCase):
     def test_csv_reader(self):
         #TODO
-        return
+        pass
--- tests/heuristic_mining_test.py
+++ tests/heuristic_mining_test.py
@@ -4,4 +4,4 @@ from mining_algorithms.heuristic_mining import HeuristicMining
 class TestHeuristic(unittest.TestCase):
     def test_heuristic_mining(self):
         #TODO
-        return
+        pass
