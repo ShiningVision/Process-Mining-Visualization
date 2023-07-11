4caa15ac53e9ffcc38b51af60884e101c5433bb3
ShiningVision <antonchen00@gmx.at>
2023-07-11 21:16:29 +0200

    Update DDCAL cluster numbers

--- mining_algorithms/ddcal_clustering.py
+++ mining_algorithms/ddcal_clustering.py
@@ -2,8 +2,11 @@ from clustering.ddcal import DDCAL
 
 class DensityDistributionClusterAlgorithm():
     def __init__(self, frequencies):
-
-        ddcal = DDCAL(n_clusters=8, feature_boundary_min=0.1, feature_boundary_max=0.49,
+        cluster_num = len(set(frequencies))
+        max_cluster = 8
+        if cluster_num>max_cluster:
+            cluster_num = max_cluster
+        ddcal = DDCAL(n_clusters=cluster_num, feature_boundary_min=0.1, feature_boundary_max=0.49,
                   num_simulations=20, q_tolerance=0.45, q_tolerance_increase_step=0.5)
         # execute DDCAL algorithm
         ddcal.fit(frequencies)
